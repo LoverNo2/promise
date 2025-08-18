c8cec2a944be682529879834ac8aa16ae10ddc94
Arca <wu.xinhai@outlook.com>
2025-08-18 17:02:46 +0800

    feat:$el&事件

--- example/app.js
+++ example/app.js
@@ -1,18 +1,33 @@
 import { h } from '../dist/index.js'
 
+window.self = null
 const app = {
   render() {
+    window.self = this
+
     return h(
       'div',
       {
         style: 'color: rgba(255, 0, 0, 0.5)',
       },
-      [h('h1', {}, this.msg), h('p', {}, 'hello vue'), h('div', {}, 'hello world')]
+      [
+        h(
+          'h1',
+          {
+            onClick() {
+              console.log('click h1')
+            },
+          },
+          this.msg
+        ),
+        h('p', {}, 'hello vue'),
+        h('div', {}, 'hello world'),
+      ]
     )
   },
   setup() {
     return {
-      msg: 'be happy',
+      msg: 'be happy!',
     }
   },
 }
